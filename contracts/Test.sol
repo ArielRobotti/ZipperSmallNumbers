c53116235d279b26534474eb34539f56bff6b0c4
ArielRobotti <arielrobotti@gmail.com>
2023-04-06 17:24:52 -0300

    Validación de array de entrada

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
-//at address - 0xD8cA28F015a89AB556698Ec17CB404964dB10c30
+// at address - 0xD8cA28F015a89AB556698Ec17CB404964dB10c30
 // Version Corregida - 0x274eCF451605F1c26DEC114F4BCBFD780c623bDa
+// Validación de entrada - 0x65d82fcF2492676a8B4D5EC1D9c39a0c50DF0eCB
 pragma solidity >=0.8.2 <0.9.0;
 import "../libs/Zipper.sol";
 
@@ -12,7 +13,7 @@ contract Test {
         compArray = _in.zip();
     }
     function len() public view returns(uint256, uint256){
-        return (compArray[0], compArray.length);
+        return (compArray.lenUnzipped(), compArray.length);
     }
     function getPosition(uint256 _pos) public view returns (uint256) {
         return compArray.readPosition(_pos);
--- libs/Zipper.sol
+++ libs/Zipper.sol
@@ -2,12 +2,20 @@
 // created by arielrobotti@gmail.com
 // at address - 0x3459e0368AaD228ff0A4198F4e78F9EC29ac914f
 // Version corregida - 0x5FDd55f8273c09833e7b32583113D6f4c455191b
+// Validacion de array - 0x68b7136F4b507dBDe03ddC1429ac58275ab5a257
 pragma solidity >=0.8.2 <0.9.0;
-library Zipper{ 
+library Zipper{
+
     function zip(uint256[] memory _values) public pure returns(uint256[] memory){
         uint256 len = _values.length;
         uint256[] memory result = new uint256[](len/32 + (len % 32 != 0 ? 1 : 0) + 1);
-        uint256 currentCluster = len;
+        uint256 firma = 97114105101108114111098111116116105064103109097105108046099111109;
+
+        uint256 currentCluster = len;   //Introducir una firma de verificacion dentro de esta variable
+                                        //para evitar que se puedan manipular uint256[] que no son 
+                                        //arrays comprimidos por esta libreria
+        currentCluster |= firma << 32;
+
         for(uint i=0; i<len; i++){
             require(_values[i]<256, "Datos de entrada invalidos");
             if (i%32 == 0){
@@ -26,13 +34,23 @@ library Zipper{
         return result;
     }
     function readPosition(uint256[] memory _data, uint256 _pos) public pure returns(uint256){
-        require(_pos < _data[0], "Posicion fuera de rango");
+        require(_data[0] >> 32 == 97114105101108114111098111116116105064103109097105108046099111109,
+                "Array incompatible");
+        require(_pos < _data[0] & 4294967295, "Posicion fuera de rango");
+        
         return _data[_pos/32+1] >> (_pos % 32 *8) & 255;
     }
     function writePosition(uint256[] storage _data, uint256 _value, uint256 _pos) public{
+        require(_data[0] >> 32 == 97114105101108114111098111116116105064103109097105108046099111109,
+                "Array incompatible");
         require(_pos<_data[0], "Posicion fuera de rango");
         require(_value<256, "Valor no permitido (0-255)");      
         _data[_pos/32+1] &= ~(255 <<  (_pos % 32 *8)); //Ponemos a cero los 8 bits correspondientes
         _data[_pos/32+1] |= _value << (_pos % 32 *8);  //Introducimos el _value en esos 8 bits
+    }
+    function lenUnzipped(uint256[] storage _data)public view returns(uint256){
+         require(_data[0] >> 32 == 97114105101108114111098111116116105064103109097105108046099111109,
+                "Array incompatible");
+        return _data[0] & 4294967295; 
     }
 }
